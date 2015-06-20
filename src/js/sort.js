43e817db8bc280fbda80e979061bf94b26cd5945
serzilo <designkota@mail.ru>
2015-06-20 13:32:37 +0300

    working sort

--- src/js/sort.js
+++ src/js/sort.js
@@ -79,6 +79,8 @@
 
 			el.css(styles);
 
+			self._changePosition(e, el);
+
 			self.defaults.moveCallback();
 		},
 		_mouseUp: function(e, el){
@@ -102,10 +104,79 @@
 
 				self.defaults.stopCallback();
 			});
+
+		},
+		_changePosition: function(e, el){
+			var self = this,
+				position = el.position(),
+				items = this.el.find(self.defaults.sortItem),
+				itemsLength = items.length;
+
+			for (var i = 0; i < itemsLength; i++){
+				var item = $(items[i]),
+					itemHeight = item.height(),
+					itemWudth = item.width(),
+					itemPosition = item.position();
+
+				if ( ((position.left < itemPosition.left) && (position.left + itemWudth > itemPosition.left)) &&
+					((position.top > itemPosition.top) && (position.top < itemHeight + itemPosition.top)) ){
+					self._swap(self.el.find('.'+self.defaults.placeholderClass), item);
+					return;
+				}
+			}
+		},
+		_swap: function(el1, el2){
+			var el1_0 = el1[0],
+				el2_0 = el2[0],
+				el1_c = el1_0.cloneNode(true),
+				el2_c = el2_0.cloneNode(true),
+				el1_0_position = el1.position(),
+				el2_0_position = el2.position(),
+				left = el2_0_position.left - el1_0_position.left,
+				top  = el2_0_position.top - el1_0_position.top;
+
+			// console.log(el1_0_position);
+			// console.log(el2_0_position);
+
+			el1_0.parentNode.replaceChild(el2_c, el1_0);
+			el2_0.parentNode.replaceChild(el1_c, el2_0);
+
+			/*
+			$(el2_c).css('transform', 'translate(' + left + 'px, ' + top + 'px)')
+					.animate({'transform' : 'translate(0px, 0px)'}, 200, 'linear', function(){
+
+					});
+			*/
+		}
+	});
+
+	$.extend(Sort, {
+		transition : function(){
+			var transition;
+
+			if('ontransitionend' in window) {
+			  // Firefox
+			  transition = 'transitionend';
+			} else if('onwebkittransitionend' in window) {
+			  // Chrome/Saf (+ Mobile Saf)/Android
+			  transition = 'webkitTransitionEnd';
+			} else if('onotransitionend' in document.body || navigator.appName == 'Opera') {
+			  // Opera
+			  // As of Opera 10.61, there is no "onotransitionend" property added to DOM elements,
+			  // so it will always use the navigator.appName fallback
+			  transition = 'oTransitionEnd';
+			} else {
+			  // IE - not implemented (even in IE9) :(
+			  transition = false;
+			}
+
+			Sort.transition = transition;
 		}
 	});
 
 	$.fn.sort = function(options){
+		Sort.transition();
+
 		return this.each(function(){
 			new Sort(this, options);
 		});
